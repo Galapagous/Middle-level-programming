f829f783621a940f1114824e320f45bbfa92fd6a
Muhammed MUsa <72840992+Galapagous@users.noreply.github.com>
2022-09-17 17:50:18 +0100

    my first commit for hello.c and README file

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main - program entry point.
+ * Description: A test program to print my name.
+ * Return: 0/nothing.
+ * */
+
+int main(void)
+{
+  printf("Muhammed Musa");
+  return(0);
+}
